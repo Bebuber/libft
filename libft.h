6cb1c1108b1d1b7469bcc168c365ec5538eab44f
Betul Buber <bebuber@1-C-14.42heilbronn.de>
2024-03-28 15:19:54 +0100

    striteri

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bebuber <bebuber@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:49:22 by bebuber           #+#    #+#             */
-/*   Updated: 2024/03/27 17:45:02 by bebuber          ###   ########.fr       */
+/*   Updated: 2024/03/28 15:16:57 by bebuber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
 char		**ft_split(char const *s, char c);
-char 		*ft_itoa(int n);
+char		*ft_itoa(int n);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
